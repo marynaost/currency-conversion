59e76d98e64e7fe602ff27551149aac9b64965af
Maryna Ostapliuk <81308158+marynaost@users.noreply.github.com>
2022-02-04 13:07:24 +0200

    fix api key

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://api.getgeoapi.com/v2/currency';
-const API_KEY = 'acee8ddc5c9a0d81ce9494362788ca5971740034';
+const API_KEY = 'a368fc6816b80ed8fe9d9588170b4de62547b896';
 
 async function fetchCurrency(url = '') {
   const response = await fetch(url);
